72d6c7e48308b62c23592ea7df949ee27eb01ae4
Enrico <enricoadi49@gmail.com>
2025-03-15 22:54:07 +0700

    .

--- src/greetings/message/Hello.groovy
+++ src/greetings/message/Hello.groovy
@@ -1,6 +1,7 @@
 package greetings.message;
 class Hello {
   static def Message(steps,String name="world"){
+    println("Hello ${name}!")
     steps.echo("Hello ${name}, nice to meet you!")
   }
 }
