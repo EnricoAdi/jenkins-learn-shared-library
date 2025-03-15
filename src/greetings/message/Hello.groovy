82b532553fdbb4d2eac7e7e70d586c90b900d1fb
Enrico <enricoadi49@gmail.com>
2025-03-15 22:53:40 +0700

    refer steps as parameter

--- src/greetings/message/Hello.groovy
+++ src/greetings/message/Hello.groovy
@@ -1,6 +1,6 @@
 package greetings.message;
 class Hello {
-  static def Message(String name="world"){
-    println("Hello ${name}, nice to meet you!")
+  static def Message(steps,String name="world"){
+    steps.echo("Hello ${name}, nice to meet you!")
   }
 }
